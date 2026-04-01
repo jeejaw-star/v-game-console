2432e72b538abaab6b0abbe9439b6784ea680d8b
jeejaw-star <jeejaw@gmail.com>
2026-04-01 09:40:39 -0400

    Added first game!

---
+++ games/guessing_v_bot1.py
@@ -0,0 +1,15 @@
+import random
+def play():
+    number=random.randint(1,100)
+    print("I'm thinking of a number between 1 and 100. I'll give you 10 tries.")
+    tries=0
+    while tries < 10:
+        guess=int(input("What's your guess?"))
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print("You got it!")
+            return True
+    return False
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from games import *
+import games.guessing_v_bot1
+from users import *
 def main():
-    pass
+    games_costs={"guessing game against bot":10}
+    user=User()
+    if user.logged_in:
+        pass
+    else:
+        while not user.logged_in:
+            user=User()
+    while True:
+        print("What game do you want to play?")
+        game=input("1: Guessing game where you guess a number a bot is thinking of. Cost: 10 credits")
+        if game == "1":
+            if user.credits >= 10:
+                user.credits-=10
+                if games.guessing_v_bot1.play():
+                    user.credits+=10
+                    print("Congratulations! You got 10 credits!")
+    
